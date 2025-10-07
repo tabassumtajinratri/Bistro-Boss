8f75f79f6d7a9f5500640be4cd6888f28c39402d
tabassumtajinratri <tabassumtajinratri@gmail.com>
2025-10-07 23:57:31 +0600

    update

--- src/Components/AdminHome/AdminHome.jsx
+++ src/Components/AdminHome/AdminHome.jsx
@@ -1,81 +1,70 @@
-// import React from 'react';
-// import useAuth from '../Hooks/useAuth';
-// import { useQuery } from '@tanstack/react-query';
-// import useAxiosSecure from '../Hooks/useAxiosSecure';
-// import { FaBook, FaDollarSign, FaJediOrder, FaUser } from 'react-icons/fa6';
 
-// const AdminHome = () => {
-//     const {user}= useAuth();
-//     const axiosSecure = useAxiosSecure()
-
-//     const {data: stats}= useQuery({
-//         queryKey: ['admin-stats'],
-//         queryFn: async()=>{
-//             const res = await axiosSecure.get('/admin-stats')
-//             return res.data
-//         }
-        
-//     })
-
-
-
-//     return (
-//         <div className='text-3xl'>
-//             <h1>Hi, Welcome</h1>
-//             {
-//                 user?.displayName ? user.displayName : 'Back'
-//             }
-
-//             <div className="stats shadow">
-//   <div className="stat">
-//     <div className="stat-figure text-secondary">
-//      <FaDollarSign></FaDollarSign>
-//     </div>
-//     <div className="stat-title">Revenue</div>
-//     <div className="stat-value">${stats?.revenue}</div>
-//     <div className="stat-desc">Jan 1st - Feb 1st</div>
-//   </div>
-
-//   <div className="stat">
-//     <div className="stat-figure text-secondary">
-//       <FaUser></FaUser>
-//     </div>
-//     <div className="stat-title"> Users</div>
-//     <div className="stat-value">{stats.users}</div>
-//     <div className="stat-desc">↗︎ 400 (22%)</div>
-//   </div>
-
-//   <div className="stat">
-//     <div className="stat-figure text-secondary">
-//    <FaJediOrder></FaJediOrder>
-//     </div>
-//     <div className="stat-title">Orders</div>
-//     <div className="stat-value">{stats.order}</div>
-//     <div className="stat-desc">↘︎ 90 (14%)</div>
-//   </div>
-
-
-//     <div className="stat">
-//     <div className="stat-figure text-secondary">
-//    <FaBook></FaBook>
-//     </div>
-//     <div className="stat-title">Menu Items</div>
-//     <div className="stat-value">{stats.menuItems}</div>
-//     <div className="stat-desc">↘︎ 90 (14%)</div>
-//   </div>
-// </div>
-//         </div>
-//     );
-// };
-
-// export default AdminHome;
-
-
-import React from 'react';
 import useAuth from '../Hooks/useAuth';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../Hooks/useAxiosSecure';
 import { FaBook, FaDollarSign, FaJediOrder, FaUser } from 'react-icons/fa6';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Pie, PieChart, ResponsiveContainer } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
 
 const AdminHome = () => {
   const { user } = useAuth();
@@ -88,6 +77,18 @@ const AdminHome = () => {
       return res.data;
     },
   });
+
+
+  const {data: chartData=[]}= useQuery({
+    queryKey: ['order-stats'],
+    queryFn: async ()=>{
+      const res= await axiosSecure.get('/order-stats');
+      return res.data;
+    }
+  })
+console.log('Order Stats (chartData):', chartData);
+
+
 
   if (!stats) {
     return <p className="text-center text-lg mt-10">Loading admin stats...</p>;
@@ -134,8 +135,41 @@ const AdminHome = () => {
           <div className="stat-desc">↘︎ 90 (14%)</div>
         </div>
       </div>
+
+      <div className="flex">
+        <div className='w-1/2'>
+          <BarChart
+      width={500}
+      height={300}
+      data={chartData}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="category" />
+      <YAxis />
+      <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+        {chartData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+          
+        ))}
+      </Bar>
+    </BarChart>
+        </div>
+
+        <div>
+
+        </div>
+      </div>
     </div>
   );
 };
 
 export default AdminHome;
+
+
+
